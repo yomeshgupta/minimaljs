18c562cba2e7a9b213252a08f49ded8d405f6a50
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-03-15 22:46:50 +0530

    Server up and running

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const CONFIG = require('./config');
 
 const server = http.createServer((req, res) => {
-	console.log(req);
+	fs.readFile(path.resolve(__dirname, 'public', 'index.html'), (err, data) => {
+		res.setHeader('Content-Type', 'text/html');
+		if (err) {
+			res.writeHead(500);
+			res.end('Some error occured');
+		}
+		res.writeHead(200);
+		return res.end(data);
+	});
 });
 
 server.listen(CONFIG.PORT, () => console.log(`Server running on ${CONFIG.PORT}`));
