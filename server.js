ae623cd9f5bcb6f92088a8dc98cdf6cdcb67e6fa
YomeshAdPushup <yomesh.gupta@adpushup.com>
2019-03-16 17:14:37 +0530

    Started working on middlewares

--- server.js
+++ server.js
@@ -2,4 +2,5 @@ const Minimal = require('./src/minimal');
 const CONFIG = require('./config');
 
 const app = new Minimal();
+
 const server = app.listen(CONFIG.PORT, () => console.log(`Server running on ${CONFIG.PORT}`));
--- src/minimal.js
+++ src/minimal.js
@@ -3,6 +3,28 @@ const fs = require('fs');
 const path = require('path');
 
 class Minimal {
+	constructor() {
+		this._middlewares = [];
+	}
+	use(...args) {
+		let path = '';
+		let handler;
+		if (args.length === 2) {
+			[path, handler] = args;
+		} else {
+			handler = args[0];
+		}
+		if (typeof path !== 'string') {
+			if (typeof path !== 'object') {
+				throw new Error('Path needs to be either a string or a regex');
+			}
+		}
+		if (typeof handler !== 'function') throw new Error('Handler needs to be a function');
+		this._middlewares.push({
+			path,
+			handler
+		});
+	}
 	listen(port = 8080, cb) {
 		return http
 			.createServer((req, res) => {
