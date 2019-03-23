2361077557bd0a5e64006636703ea61d43a8c725
YomeshAdPushup <yomesh.gupta@gmail.com>
2019-03-23 14:34:03 +0530

    Added use method

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const Minimal = require('./src/minimal');
+const minimal = require('./src/minimal');
 const CONFIG = require('./config');
 
-const app = new Minimal();
+const app = minimal();
 
 const server = app.listen(CONFIG.PORT, () => console.log(`Server running on ${CONFIG.PORT}`));
--- src/minimal.js
+++ src/minimal.js
@@ -2,36 +2,31 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-class Minimal {
-	constructor() {
-		this._middlewares = [];
-	}
-	use(...args) {
-		let path = '';
-		let handler;
-		if (args.length === 2) {
-			[path, handler] = args;
-		} else {
-			handler = args[0];
-		}
-		if (typeof path !== 'string') {
-			if (typeof path !== 'object') {
-				throw new Error('Path needs to be either a string or a regex');
-			}
-		}
-		if (typeof handler !== 'function') throw new Error('Handler needs to be a function');
-		this._middlewares.push({
+function Minimal() {
+	const _middlewares = [];
+
+	function use(...args) {
+		let path = '*';
+		let handler = null;
+
+		if (args.length === 2) [path, handler] = args;
+		else handler = args[0];
+
+		if (typeof path !== 'string') throw new Error('Path needs to be either a string');
+		else if (typeof handler !== 'function') throw new Error('Middleware needs to be a function');
+
+		_middlewares.push({
 			path,
 			handler
 		});
 	}
-	listen(port = 8080, cb) {
+
+	function listen(port = 8080, cb) {
 		return http
 			.createServer((req, res) => {
 				fs.readFile(path.resolve(__dirname, '../', 'public', 'index.html'), (err, data) => {
 					res.setHeader('Content-Type', 'text/html');
 					if (err) {
-						console.log(err);
 						res.writeHead(500);
 						res.end('Some error occured');
 					}
@@ -40,12 +35,19 @@ class Minimal {
 				});
 			})
 			.listen({ port }, () => {
-				if (typeof cb !== 'function') {
+				if (cb) {
+					if (typeof cb === 'function') {
+						return cb();
+					}
 					throw new Error('Listen callback needs to be a function');
 				}
-				cb();
 			});
 	}
+
+	return {
+		use,
+		listen
+	};
 }
 
 module.exports = Minimal;
